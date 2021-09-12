c8c021f53b836dcf1a90f6fc908b7b8aff1231d8
nitishrandive <40311415+nitishrandive@users.noreply.github.com>
2021-09-12 22:36:21 +0530

    adding Header/Project/Dashboard component

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import Header from './Layout/Header';
+import Projectitem from './Project/Projectitem';
 
 class Dashboard extends Component {
     render() {
         return (
-            
+            <div>
+            <Header/>
             <h1>Welcome to Dashboard !!</h1>
-
+            <Projectitem/>
+            <Projectitem/>
+            <Projectitem/>
+            </div>
         )
     }
 }
